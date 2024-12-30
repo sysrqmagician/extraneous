993763e11b93852bbce7cf98b11e6d64e9688d60
sysrqmagician <sysrqmagician@proton.me>
2024-12-30 03:52:57 +0100

    feat: error handling for parser

--- src/content.ts
+++ src/content.ts
@@ -1,11 +1,19 @@
 console.log("Content script loaded");
 
+class VideoParseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "VideoParseError";
+  }
+}
+
+type Option<T> = T | null;
 type Video = {
   title: string;
   channelName: string;
   channelLink: string;
   videoLink: string;
-  duration: string;
+  duration: Option<string>; // Is not sent for Livestreams and on certain pages
   element: HTMLDivElement;
 };
 
@@ -26,16 +34,26 @@ function injectScript() {
 }
 
 function parseFeed(video_div: HTMLDivElement): Video {
-  const title =
-    video_div.querySelector("div.video-card-row > a > p")?.textContent || "";
+  const titleElement = video_div.querySelector("div.video-card-row > a > p");
+  if (!titleElement?.textContent)
+    throw new VideoParseError("Could not find video title");
+  const title = titleElement.textContent;
+
   const channelLinkElement = video_div.querySelector("a[href^='/channel/']");
-  const channelName = (channelLinkElement?.textContent || "").trim();
-  const channelLink = channelLinkElement?.getAttribute("href") || "";
-  const videoLink =
-    video_div.querySelector("div.thumbnail > a")?.getAttribute("href") || "";
+  if (!channelLinkElement?.textContent)
+    throw new VideoParseError("Could not find channel name");
+  const channelName = channelLinkElement.textContent.trim();
+
+  const channelLink = channelLinkElement.getAttribute("href");
+  if (!channelLink) throw new VideoParseError("Could not find channel link");
+
+  const videoLinkElement = video_div.querySelector("div.thumbnail > a");
+  const videoLink = videoLinkElement?.getAttribute("href");
+  if (!videoLink) throw new VideoParseError("Could not find video link");
+
   const duration =
     video_div.querySelector("div.thumbnail > div > p.length")?.textContent ||
-    "";
+    null;
 
   return {
     title,
@@ -48,16 +66,26 @@ function parseFeed(video_div: HTMLDivElement): Video {
 }
 
 function parseWatchNext(video_div: HTMLDivElement): Video {
-  const title =
-    video_div.querySelector("div.video-card-row > a > p")?.textContent || "";
+  const titleElement = video_div.querySelector("div.video-card-row > a > p");
+  if (!titleElement?.textContent)
+    throw new VideoParseError("Could not find video title");
+  const title = titleElement.textContent;
+
   const channelLinkElement = video_div.querySelector("a[href^='/channel/']");
-  const channelName = (channelLinkElement?.textContent || "").trim();
-  const channelLink = channelLinkElement?.getAttribute("href") || "";
-  const videoLink =
-    video_div.querySelector("div.thumbnail > a")?.getAttribute("href") || "";
+  if (!channelLinkElement?.textContent)
+    throw new VideoParseError("Could not find channel name");
+  const channelName = channelLinkElement.textContent.trim();
+
+  const channelLink = channelLinkElement.getAttribute("href");
+  if (!channelLink) throw new VideoParseError("Could not find channel link");
+
+  const videoLinkElement = video_div.querySelector("div.thumbnail > a");
+  const videoLink = videoLinkElement?.getAttribute("href");
+  if (!videoLink) throw new VideoParseError("Could not find video link");
+
   const duration =
     video_div.querySelector("div.thumbnail > div > p.length")?.textContent ||
-    "";
+    null;
 
   return {
     title,
