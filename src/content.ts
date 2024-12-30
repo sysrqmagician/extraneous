55e623f367e1e3442d2ff6d3924544f71a0c4109
sysrqmagician <sysrqmagician@proton.me>
2024-12-30 03:46:58 +0100

    feat: basic video recommendations parsing

--- src/content.ts
+++ src/content.ts
@@ -1,9 +1,72 @@
 console.log("Content script loaded");
 
+type Video = {
+  title: string;
+  channelName: string;
+  channelLink: string;
+  videoLink: string;
+  duration: string;
+  element: HTMLDivElement;
+};
+
 function injectScript() {
-  const div = document.createElement("div");
-  div.textContent = "Injected by extension!";
-  document.body.appendChild(div);
+  let videos: Array<Video> = [];
+  const video_iterator = document
+    .querySelectorAll("div[class = 'video-card-row']")
+    .values()
+    .map((x) => x.parentElement as HTMLDivElement);
+
+  if (document.location.pathname === "/watch") {
+    videos = Array.from(video_iterator.map((x) => parseWatchNext(x)));
+  } else if (document.location.pathname.startsWith("/feed")) {
+    videos = Array.from(video_iterator.map((x) => parseFeed(x)));
+  }
+
+  console.log(videos);
+}
+
+function parseFeed(video_div: HTMLDivElement): Video {
+  const title =
+    video_div.querySelector("div.video-card-row > a > p")?.textContent || "";
+  const channelLinkElement = video_div.querySelector("a[href^='/channel/']");
+  const channelName = (channelLinkElement?.textContent || "").trim();
+  const channelLink = channelLinkElement?.getAttribute("href") || "";
+  const videoLink =
+    video_div.querySelector("div.thumbnail > a")?.getAttribute("href") || "";
+  const duration =
+    video_div.querySelector("div.thumbnail > div > p.length")?.textContent ||
+    "";
+
+  return {
+    title,
+    channelName,
+    channelLink,
+    videoLink,
+    duration,
+    element: video_div,
+  };
+}
+
+function parseWatchNext(video_div: HTMLDivElement): Video {
+  const title =
+    video_div.querySelector("div.video-card-row > a > p")?.textContent || "";
+  const channelLinkElement = video_div.querySelector("a[href^='/channel/']");
+  const channelName = (channelLinkElement?.textContent || "").trim();
+  const channelLink = channelLinkElement?.getAttribute("href") || "";
+  const videoLink =
+    video_div.querySelector("div.thumbnail > a")?.getAttribute("href") || "";
+  const duration =
+    video_div.querySelector("div.thumbnail > div > p.length")?.textContent ||
+    "";
+
+  return {
+    title,
+    channelName,
+    channelLink,
+    videoLink,
+    duration,
+    element: video_div,
+  };
 }
 
 // Run when document is loaded
